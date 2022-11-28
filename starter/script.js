d563e1b22a1a3f62b8bebe81805d8dff1e811ab0
Aseneca <princeosuh@gmail.com>
2022-11-28 20:26:57 +0100

    implement close account; implemnet loan request

--- starter/script.js
+++ starter/script.js
@@ -152,7 +152,7 @@ btnLogin.addEventListener('click', function (e) {
   currAccount = accounts.find(function (acc) {
     return acc.username === inputLoginUsername.value;
   });
-  console.log(currAccount);
+  // console.log(currAccount);
   if (currAccount?.pin === Number(inputLoginPin.value)) {
     //Display the UI
     labelWelcome.textContent = `WELCOME ${currAccount.owner.split(' ')[0]}`;
@@ -188,9 +188,44 @@ btnTransfer.addEventListener('click', function (e) {
   }
   updateUi(currAccount);
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  //check loan condition
+  if (
+    amount > 0 &&
+    currAccount.transactions.some(trans => trans >= amount * 0.1)
+  ) {
+    //credit loan
+    currAccount.transactions.push(amount);
+    //update UI
+    updateUi(currAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    currAccount.username === inputCloseUsername.value &&
+    currAccount.pin === Number(inputClosePin.value)
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  } else {
+    console.log('wrong cred');
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+console.log(accounts);
 ////////////////////////////////////////////////////////
-// const testData1 = [5, 2, 4, 1, 15, 8, 3];
-// const testData2 = [16, 6, 10, 5, 6, 1, 4];
+const testData1 = [5, 2, 4, 1, 15, 8, 3];
+const testData2 = [16, 6, 10, 5, 6, 1, 4];
 // const calcAverageHumanAge = function (arr) {
 //   let humanAge;
 //   let filteredAges;
@@ -200,7 +235,6 @@ btnTransfer.addEventListener('click', function (e) {
 //   console.log(humanAge);
 //   filteredAges = humanAge.filter(filteredAge => filteredAge >= 18);
 //   console.log(filteredAges);
-
 //   avgHumanAges = filteredAges.reduce(
 //     (acc, avgHumanAge) => acc + avgHumanAge / arr.length,
 //     0
@@ -209,3 +243,73 @@ btnTransfer.addEventListener('click', function (e) {
 // };
 // calcAverageHumanAge(testData1);
 // calcAverageHumanAge(testData2);
+// const calcAverageHumanAge = ages =>
+//   ages
+//     .map(Age => (Age <= 2 ? Age * 2 : 16 + Age * 4))
+//     .filter(filteredAge => filteredAge >= 18)
+//     .reduce(
+//       (acc, avgHumanAge, index, arr) => acc + avgHumanAge / arr.length,
+//       0
+//     );
+
+// const avg1 = calcAverageHumanAge(testData1);
+// const avg2 = calcAverageHumanAge(testData2);
+// console.log(avg1, avg2);
+
+// const dogs = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 32, curFood: 340, owners: ['Michael'] },
+// ];
+// let recommendedFood;
+// const calcRecommendFood = function (dog) {
+//   for (const value of dog) {
+//     value['recommendedFood'] = `${value['weight'] ** 0.75 * 28}g`;
+//   }
+// };
+// calcRecommendFood(dogs);
+
+// const findDog = function (arr) {
+//   arr.find(function (dog, i) {
+//     dog.owners[i] === 'Maltida';
+//     if (dog.curFood > dog.recommendedFood) {
+//       console.log(`eating much`);
+//     } else {
+//       console.log(`eating little`);
+//     }
+//   });
+// };
+// findDog(dogs);
+// console.log(dogs);
+
+// Julia and Kate are still studying dogs, and this time they are studying if dogs are
+// eating too much or too little.
+// Eating too much means the dog's current food portion is larger than the
+// recommended portion, and eating too little is the opposite.
+// Eating an okay amount means the dog's current food portion is within a range 10%
+// above and 10% below the recommended portion (see hint).
+// Your tasks:
+// 1. Loop over the 'dogs' array containing dog objects, and for each dog, calculate
+// the recommended food portion and add it to the object as a new property. Do
+// not create a new array, simply loop over the array. Forumla:
+// recommendedFood = weight ** 0.75 * 28. (The result is in grams of
+// food, and the weight needs to be in kg)
+// 2. Find Sarah's dog and log to the console whether it's eating too much or too
+// little. Hint: Some dogs have multiple owners, so you first need to find Sarah in
+// the owners array, and so this one is a bit tricky (on purpose) �
+// 3. Create an array containing all owners of dogs who eat too much
+// ('ownersEatTooMuch') and an array with all owners of dogs who eat too little
+// ('ownersEatTooLittle').
+// 4. Log a string to the console for each array created in 3., like this: "Matilda and
+// Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat
+// too little!"
+// 5. Log to the console whether there is any dog eating exactly the amount of food
+// that is recommended (just true or false)
+// 6. Log to the console whether there is any dog eating an okay amount of food
+// (just true or false)
+// 7. Create an array containing the dogs that are eating an okay amount of food (try
+// to reuse the condition used in 6.)
+// 8. Create a shallow copy of the 'dogs' array and sort it by recommended food
+// portion in an ascending order (keep in mind that the portions are inside the
+// array's objects �
